12a14cb5e498c25f6892a145612c34ceeafdcc82
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-10-10 00:08:00 -0300

    finished sign up page

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, ImageBackground, Text} from 'react-native';
+
+import style from "./style";
+import bgImg from "../../assets/images/give-classes-background.png";
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 function GiveClasses(){
-    return <View />
+    const {goBack} = useNavigation();
+
+    function navigateBack(){
+        goBack();
+    }
+    return (
+    <View style={style.container}>
+        <ImageBackground resizeMode='contain' source={bgImg} style={style.content}>
+            <Text style={style.tittle}>Quer ser um proffy?</Text>
+            <Text style={style.description}>
+                Para começar, você precisa se cadastrar na nossa plataforma web. 
+            </Text>
+        </ImageBackground>
+        <RectButton 
+        onPress={navigateBack}
+        style={style.okButton}>
+            <Text style={style.okText}>Tudo bem</Text>
+        </RectButton>
+    </View>
+    );
 }
 
 export default GiveClasses; 
