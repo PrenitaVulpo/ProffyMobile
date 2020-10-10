41e1a9687321c3274ab4e3eec33223d9ce198ef5
PrenitaVulpo <guilhermepdl97@protonmail.com>
2020-10-10 00:20:47 -0300

    Added Favorites and ProfList pages

---
+++ src/pages/ProfList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import style from './style';
+
+function ProfList(){
+    return (
+        <View>
+            
+        </View>
+    )
+};
+
+export default ProfList;
